fd77113a54fd3e29fd884857803c3e3c7cc6d07c
Ardy Setiawan <Ardy_S458@mitrais.com>
2018-09-12 15:58:56 +0800

    first commit

---
+++ php/index.php
@@ -0,0 +1,79 @@
+<?php
+
+$i = "ardy";
+$a = "ardy";
+echo "asdasda".$i.$a;
+
+
+function test($a,$i){
+    return $a."-".$i;
+}
+
+echo test($a,$i);
+
+$i = "ardy";
+$a = "ardy";
+echo "asdasda".$i.$a;
+
+
+function test($a,$i){
+    return $a."-".$i;
+}
+
+echo test($a,$i);
+
+$i = "ardy";
+$a = "ardy";
+echo "asdasda".$i.$a;
+
+
+function test($a,$i){
+    return $a."-".$i;
+}
+
+echo test($a,$i);
+
+$i = "ardy";
+$a = "ardy";
+echo "asdasda".$i.$a;
+
+
+function test($a,$i){
+    return $a."-".$i;
+}
+
+echo test($a,$i);
+
+$i = "ardy";
+$a = "ardy";
+echo "asdasda".$i.$a;
+
+
+function test($a,$i){
+    return $a."-".$i;
+}
+
+echo test($a,$i);
+
+$i = "ardy";
+$a = "ardy";
+echo "asdasda".$i.$a;
+
+
+function test($a,$i){
+    return $a."-".$i;
+}
+
+echo test($a,$i);
+
+$i = "ardy";
+$a = "ardy";
+echo "asdasda".$i.$a;
+
+
+function test($a,$i){
+    return $a."-".$i;
+}
+
+echo test($a,$i);
+?>
